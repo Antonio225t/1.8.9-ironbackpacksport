1fe8243035c935a72562838d1967dbd7454cf03e
GitHub Action <action@github.com>
2025-01-05 20:32:27 +0000

    Initialize template repository proper
    Set package name to com.github.antonio225t.189ironbackpacksport
    Set project name to 1.8.9-ironbackpacksport
    Set modid to ironbackpacksport

--- src/main/java/com/github/antonio225t/189ironbackpacksport/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/antonio225t/189ironbackpacksport/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.antonio225t.189ironbackpacksport.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/antonio225t/189ironbackpacksport/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/antonio225t/189ironbackpacksport/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.antonio225t.189ironbackpacksport.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
